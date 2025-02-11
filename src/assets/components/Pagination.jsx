d4c7f98d6b5ff0995e787ffdc7e9c6d43088e5f5
Cédric Truyen <cedrictruyen@icloud.com>
2025-02-11 10:42:32 +0100

    layout pagination

--- src/assets/components/Pagination.jsx
+++ src/assets/components/Pagination.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Pagination() {
   return (
-    <div>Pagination</div>
+    <div className='pagination'>
+      <select className='form-control size select'>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+      </select>
+
+      <button className='btn'>1</button>
+      <button className='btn'>2</button>
+      <button className='btn active'>3</button>
+      <button className='btn'>4</button>
+      <button className='btn'>5</button>
+    </div>
   )
 }
