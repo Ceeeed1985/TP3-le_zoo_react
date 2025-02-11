19bb1c8cb863ababb3da0067ad4dc756fd57c632
Cédric Truyen <cedrictruyen@icloud.com>
2025-02-11 10:26:31 +0100

    creation animalsCards

--- src/assets/components/AnimalItem.jsx
+++ src/assets/components/AnimalItem.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
-export default function AnimalItem() {
+export default function AnimalItem({ animal }) {
+
+  const cardImgStyle = {
+    backgroundImage: `url(assets/images/${animal.image})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '300px',
+    borderTopLeftRadius: '5px',
+    borderTopRightRadius: '5px',
+  };
+
   return (
-    <div>Carte avec chaque animaux</div>
+    <div className='card'>
+      <div style={cardImgStyle}></div>
+      <div className='card-body'>
+        <div className='card-title'>{animal.nom}</div>
+        <div className='badge-success'>{animal.categorie}</div>
+      </div>
+    </div>
   )
 }
--- src/assets/components/PageContainer.jsx
+++ src/assets/components/PageContainer.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import Search from './Search';
 import CategorieList from './CategorieList';
-import AnimalItem from './AnimalItem';
 import Pagination from './Pagination';
+import AnimalList from './AnimalList';
+import { allData } from '../../../mocks/data-animals';
 
 export default function PageContainer() {
+
+  const [animals, setAnimals] = useState(allData);
+
   return (
     <div>
         <Header />
-        <div className='appContent'>
+        <div className='appContent container'>
           <Search />
           <CategorieList />
-          <AnimalItem />
+          <AnimalList animals={animals}/>
           <Pagination />
         </div>
     </div>
