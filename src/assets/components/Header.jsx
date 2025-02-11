74f19c48990a159a3fc8989ffd03cb3d9fd4ecd7
Cédric Truyen <cedrictruyen@icloud.com>
2025-02-11 09:25:09 +0100

    Add Header + navbar

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div>Contient le bloc top et la navigation <br /></div>
+    <>
+      <div className='top-image-container'></div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Le Zoo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Les plus populaires</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
